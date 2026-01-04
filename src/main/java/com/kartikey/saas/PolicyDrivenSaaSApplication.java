4a5d075fb7728b6df09c43dfcdebaed731e5f884
Kartikey027 <saxenakartikey208@gmail.com>
2026-01-04 16:53:34 +0530

    Initial Local Project Setup

---
+++ src/main/java/com/kartikey/saas/PolicyDrivenSaaSApplication.java
@@ -0,0 +1,13 @@
+package com.kartikey.saas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PolicyDrivenSaaSApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PolicyDrivenSaaSApplication.class, args);
+	}
+
+}
